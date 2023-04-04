11d995aea6eba18058f4fbc740549c0506d511a8
Tristan-1123 <112727293+Tristan-1123@users.noreply.github.com>
2023-04-04 08:24:39 -0500

    4/4

---
+++ HelloName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+
+            Console.Write("enter your age: ");
+            int age = int.Parse(Console.)
+
+            Console.ReadKey();
+        }
+    }
+}
