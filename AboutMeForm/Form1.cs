7c13e3f7a3773bfe8aa2fa6a8ed9d0bdee5db0d8
Tristan-1123 <112727293+Tristan-1123@users.noreply.github.com>
2023-04-14 08:07:31 -0500

    up

---
+++ AboutMeForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboutMeForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "My name is Tristan.";
+            label2.Text = "My favorite sport is Football.";
+            label3.Text = "There is no security on this earth, only opportunity.";
+        }
+    }
+}
