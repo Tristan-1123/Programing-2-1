a733f8afcce64a3174fcfe6718cf97c636460530
Tristan-1123 <112727293+Tristan-1123@users.noreply.github.com>
2023-05-23 08:06:22 -0500

    up

---
+++ StringInterveiw18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterveiw18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String. ");
+            string str = Console.ReadLine();
+            Console.Write("Enter the Letter You Wish To Remove. ");
+            string rem = Console.ReadLine();
+            char remove = char.Parse(rem);
+            string answer = "";
+
+            for (int num = 0; num < str.Length; num++)
+            {
+                if (str[num] != remove)
+                    answer += str[num];
+            }
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
